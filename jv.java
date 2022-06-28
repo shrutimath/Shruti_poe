bafd510404472ab78c7b215505884a04a92eac01
shrutimath <79987977+shrutimath@users.noreply.github.com>
2022-06-28 11:46:16 +0530

    Update jv.java

--- jv.java
+++ jv.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class OddEven
+{
+  public static void main(String args[])
+  {
+	Scanner ob = new Scanner(System.in);
+	int i;
+	System.out.println("Enter any integer to check whether its odd or even...");
+	i = ob.nextInt();
+	if(i%2==0)
+	{
+		System.out.println("You entered an even number");
+	}
+  }
+}
